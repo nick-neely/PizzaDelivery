ec2cce8c0a828865ecd6947e4a64f884658a974a
LoganBerndt <116930335+LoganBerndt@users.noreply.github.com>
2023-12-04 11:23:45 -0600

    Added Pizza.cs class

---
+++ Pizza.cs
@@ -0,0 +1,46 @@
+﻿namespace BlazorApp
+{
+	public class Pizza
+	{
+		public Topping PizzaTopping { get; set; }
+
+		public Pizza()
+		{
+			PizzaTopping = new Topping();
+		}
+
+		public decimal GetFinalPrice()
+		{
+			return PizzaTopping.TopppingPrice();
+		}
+	}
+
+	public class Topping
+	{
+		public bool Bacon { get; set; }
+
+		public bool MoreBacon { get; set; }
+
+		public bool EvenMoreBacon { get; set; }
+
+		public decimal TopppingPrice()
+		{
+			decimal runningTotal = 0;
+
+			if (Bacon)
+			{
+				runningTotal += 30;
+			}
+			if (MoreBacon)
+			{
+				runningTotal += 50;
+			}
+			if (EvenMoreBacon)
+			{
+				runningTotal += 200;
+			}
+
+			return runningTotal;
+		}
+	}
+}
